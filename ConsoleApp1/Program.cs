64fb02f1f92a4a8f72ea85e11739577e009d7356
admin <admin@CHETUIWK938>
2022-03-07 18:00:01 +0530

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Emp
+    {
+      private  string name = "manoj";
+       private int age = 32;
+        
+       
+        static void Main(string[] args)
+        {
+            Emp p = new Emp();
+            Console.WriteLine("name"  + p.name);
+            Console.WriteLine("age"  + p.age);
+            Console.ReadLine();
+        }
+
+       
+           
+
+
+    }
+        }
+    
+
